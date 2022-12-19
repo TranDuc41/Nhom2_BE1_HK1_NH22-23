3241f866cada951ce79ebd9811aedbe06f1f4676
BaoDuongVan <baoharry101@gmail.com>
2022-12-19 23:18:29 +0700

    update customer

--- models/product.php
+++ models/product.php
@@ -15,7 +15,7 @@ class Product extends Db
      {
          $sql = self::$connection->prepare("SELECT * FROM products, manufactures, protypes
          WHERE products.manu_id = manufactures.manu_id
-         AND products.type_id = protypes.type_id 
+         AND products.type_id = protypes.type_id
          ORDER BY `id` DESC ");
          $sql->execute(); //return object
          $item = array();
